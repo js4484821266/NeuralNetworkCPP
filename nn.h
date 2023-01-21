e1415eb6395dbd5beeec513d346df1bd03b1136e
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2023-01-22 05:35:52 +0900

    compose

--- nn.h
+++ nn.h
@@ -1,1 +1,24 @@
 #pragma once
+#include<ctgmath>
+#include <vector>
+
+namespace js4484821266
+{
+	inline long double sigmoid(const long double x)
+	{
+		return 1 / (1 + exp(-x));
+	}
+
+	class hidden_layer
+	{
+	public:
+		hidden_layer(size_t n = 2, long double (*f)(long double) = sigmoid)
+		{
+		}
+	};
+
+	class nn
+	{
+	public:
+	};
+}
