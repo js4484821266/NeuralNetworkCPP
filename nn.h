86d433039a4a5f70aa32dcdb30052a3b8897b978
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2023-01-22 21:46:18 +0900

    remove delta and epsilon

--- nn.h
+++ nn.h
@@ -4,18 +4,6 @@
 
 namespace js4484821266
 {
-	inline long double delta(
-		const long double x
-	)
-	{
-		long double t = 1, u = 0;
-		while (x + (u = t / 2) > x)
-			t = u;
-		return t;
-	}
-
-	long double epsilon = delta(0);
-
 	inline long double sigmoid(const long double x)
 	{
 		return 1 / (1 + exp(-x));
