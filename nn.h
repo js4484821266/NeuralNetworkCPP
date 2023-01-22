693ff0e89785e6e5232492b8cd3ec8675a942f92
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2023-01-22 17:42:36 +0900

    major change

--- nn.h
+++ nn.h
@@ -4,21 +4,35 @@
 
 namespace js4484821266
 {
+	inline long double delta(
+		const long double x
+	)
+	{
+		long double t = 1, u = 0;
+		while (x + (u = t / 2) > x)
+			t = u;
+		return t;
+	}
+
+	long double epsilon = delta(0);
+
 	inline long double sigmoid(const long double x)
 	{
 		return 1 / (1 + exp(-x));
 	}
 
+	inline long double analytic_d_dx(
+		long double (*f)(long double),
+		const long double x
+	)
+	{
+		return (f(x + delta(x)) - f(x)) / delta(x);
+	}
+
 	class hidden_layer
 	{
 	public:
-		hidden_layer(size_t n = 2, long double (*f)(long double) = sigmoid)
-		{
-		}
-	};
-
-	class nn
-	{
-	public:
+		std::vector<std::vector<long double>> w;
+		long double (*f)(long double) = sigmoid;
 	};
 }
