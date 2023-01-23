163652669a145f797a066c1b362368b55739f250
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2023-01-23 14:04:51 +0900

    declare f(x) and test d_dx with exp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include<iostream>
 #include "nn.h"
 
+long double f(const long double x)
+{
+	return 1 / x;
+}
+
 int main(void)
 {
 	std::cout
 		<< js4484821266::d_dx(
-			sqrt,
-			2
+			exp,
+			1.
 		)
 		<< std::endl;
 	return 0;
