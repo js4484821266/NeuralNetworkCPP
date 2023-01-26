6217b24beb86a75de9f2e8be2475c6ffa9d7730d
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2023-01-27 06:15:37 +0900

    class layer

--- nn.h
+++ nn.h
@@ -1,5 +1,6 @@
 #pragma once
 #include<ctgmath>
+#include<random>
 #include <vector>
 
 namespace js4484821266
@@ -8,4 +9,18 @@ namespace js4484821266
 	{
 		return 1 / (1 + exp(-x));
 	}
+
+	class layer
+	{
+	public:
+		std::vector<std::vector<long double>>w;
+		std::vector<long double>z;
+		long double(*f)(long double) = sigmoid;
+		layer* next = nullptr;
+		layer(const size_t n_levels=1)
+		{
+			// TODO
+			if (n_levels > 1)next = new layer(n_levels - 1);
+		}
+	};
 }
