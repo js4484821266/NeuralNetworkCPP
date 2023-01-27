8a41f6dfbff21ae8e3b5d382e09addce2f8d7927
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2023-01-27 09:31:53 +0900

    clear

--- nn.h
+++ nn.h
@@ -9,18 +9,4 @@ namespace js4484821266
 	{
 		return 1 / (1 + exp(-x));
 	}
-
-	class layer
-	{
-	public:
-		std::vector<std::vector<long double>>w;
-		std::vector<long double>z;
-		long double(*f)(long double) = sigmoid;
-		layer* next = nullptr;
-		layer(const size_t n_levels=1)
-		{
-			// TODO
-			if (n_levels > 1)next = new layer(n_levels - 1);
-		}
-	};
 }
